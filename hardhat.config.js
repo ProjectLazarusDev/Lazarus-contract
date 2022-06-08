4eeb7a11158418ef1a0cbea73e36c87f87b5f058
neoqiwen <neoqiwen@gmail.com>
2022-06-09 01:45:27 +0800

    updated hardhat for live deployment with arbitrum testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,19 @@
+// alchemyApiKey 
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+// mnemonic 
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
 require("@nomiclabs/hardhat-waffle");
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.13",
+  networks: {
+    arbitrum_rinkeby: {
+      url: `https://rinkeby.arbitrum.io/rpc/${alchemyApiKey}`,
+      accounts: [`${mnemonic}`]
+    }
+  }
 };
